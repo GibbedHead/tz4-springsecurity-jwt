f6c1a8e59d191721939e44865dc0206204e259f6
GibbedHead <chaplygin-ma@yandex.ru>
2024-06-02 19:06:30 +0300

    Шаблон проекта spring boot

---
+++ src/main/java/ru/chaplyginma/SpringSecurityJwtHomework/SpringSecurityJwtHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.chaplyginma.SpringSecurityJwtHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/chaplyginma/SpringSecurityJwtHomework/SpringSecurityJwtHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chaplyginma.SpringSecurityJwtHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
