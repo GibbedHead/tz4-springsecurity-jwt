2c1adcdf8f48f6e2d5c7a1fb58a3c043c891a6ac
GibbedHead <chaplygin-ma@yandex.ru>
2024-06-04 21:51:21 +0300

    Причесал отступы

--- src/main/java/ru/chaplyginma/SpringSecurityJwtHomework/SpringSecurityJwtHomeworkApplication.java
+++ src/main/java/ru/chaplyginma/SpringSecurityJwtHomework/SpringSecurityJwtHomeworkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityJwtHomeworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtHomeworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtHomeworkApplication.class, args);
+    }
 
 }
