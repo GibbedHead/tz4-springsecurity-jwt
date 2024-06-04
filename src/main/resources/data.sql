6765531bf91d7834f57a78a1d0119f8a8629571e
GibbedHead <chaplygin-ma@yandex.ru>
2024-06-04 21:52:22 +0300

    Перенес стандартных пользователя и админа в миграцию первую

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "users" (user_name, password, email)
-VALUES ('user', '$2a$12$5CRiwtHPnPp4mM2Yi0qJSedpd3QNDWAH.vFIQOAGgSfncw60uOjua', 'user@domain.com'),
-       ('admin', '$2a$12$Q5qVXBrBpF9FMJyK7b8fnOyuHVxgKwIfFscOySl5JhUFX49F/Q0W2', 'admin@domain.com');
-
-INSERT INTO user_role
-VALUES (1, 1),
-       (2, 2);
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -35,3 +35,11 @@ CREATE TABLE refresh_tokens
 INSERT INTO roles (name)
 VALUES ('ROLE_USER'),
        ('ROLE_ADMIN');
+
+INSERT INTO "users" (user_name, password, email)
+VALUES ('user', '$2a$12$5CRiwtHPnPp4mM2Yi0qJSedpd3QNDWAH.vFIQOAGgSfncw60uOjua', 'user@domain.com'),
+       ('admin', '$2a$12$Q5qVXBrBpF9FMJyK7b8fnOyuHVxgKwIfFscOySl5JhUFX49F/Q0W2', 'admin@domain.com');
+
+INSERT INTO user_role
+VALUES (1, 1),
+       (2, 2);
